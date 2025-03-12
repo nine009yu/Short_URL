61f0331edb9083a7d3ec9b8996b04b0502879f3f
peeranat009 <159885131+peeranat009@users.noreply.github.com>
2025-03-12 15:18:31 +0700

    Create

---
+++ backend/server.js
@@ -0,0 +1,20 @@
+const express=require('express')
+const mysql=require('mysql')
+const cors=require('cors')
+port=8000
+
+const app=express()
+app.use(cors())
+app.use(express.json()) 
+
+// const db = mysql.createConnection({
+//     host:"localhost",
+//     user:'root',
+//     password:'',
+//     database:'testemwork'
+
+// })
+
+app.listen(port,()=>{
+    console.log("listening "+port)
+})
---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='text-center'>
+      <h1>nine</h1>
+    </div>
+  )
+}
+
+export default App
